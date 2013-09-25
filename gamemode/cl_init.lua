6b7f8cc0f04ef08638da52e3315b476cc8eec350
CW <cyber_cam34@yahoo.com.au>
2013-09-25 15:01:33 +1000

    Added knife/gun glow

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -44,3 +44,24 @@ function GM:PostDrawTranslucentRenderables()
 	self:DrawFootprints()
 end
 
+function GM:PreDrawHalos()
+
+	local entL = ents.FindByClass( "weapon_mu_magnum" )
+	for k,v in pairs(entL) do
+		if IsValid(v.Owner) then
+			entL[k] = nil
+		end
+	end
+	halo.Add(entL, Color(0, 0, 255), 5, 5, 5, true, false)
+
+	if self:GetAmMurderer() then
+		local knives = ents.FindByClass( "weapon_mu_knife" )
+		for k,v in pairs(knives) do
+			if IsValid(v.Owner) then
+				knives[k] = nil
+			end
+		end
+		halo.Add(knives, Color(220, 0, 0), 5, 5, 5, true, false)
+		halo.Add(ents.FindByClass( "mu_knife" ), Color(220, 0, 0), 5, 5, 5, true, false)
+	end
+end
