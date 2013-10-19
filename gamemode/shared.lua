543b2e846293c94d164b6d6548d60b083473801f
CW <cyber_cam34@yahoo.com.au>
2013-10-19 12:53:27 +1100

    Updated stuff

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,8 +1,9 @@
 GM.Name 	= "Murder"
 GM.Author 	= "MechanicalMind"
 // credits to Minty Fresh for some styling on the scoreboard
+// credits to Waddlesworth for the logo and menu icon
 GM.Email 	= ""
-GM.Website 	= ""
+GM.Website 	= "www.codingconcoctions.com/murder/"
 
 team.SetUp(1, "Spectators", Color(150, 150, 150))
 team.SetUp(2, "Players", Color(26, 120, 245))
