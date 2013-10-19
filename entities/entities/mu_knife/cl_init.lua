446ff3dca6c9bcf571872c6f4e0e42d5ed270164
CW <cyber_cam34@yahoo.com.au>
2013-10-19 14:15:19 +1100

    Added smoke trail on knife

--- entities/entities/mu_knife/cl_init.lua
+++ entities/entities/mu_knife/cl_init.lua
@@ -2,9 +2,40 @@
 include('shared.lua')
 
 function ENT:Initialize()
-
+	self.Emitter = ParticleEmitter(self:GetPos())
+	self.NextPart = CurTime() 
 end
 
 function ENT:Draw()
 	self:DrawModel()
+end
+
+function ENT:Think()
+	local pos = self:GetPos()
+	local client = LocalPlayer()
+
+	if self.NextPart < CurTime() then
+
+		if client:GetPos():Distance(pos) > 1000 then return end
+
+		self.Emitter:SetPos(pos)
+		self.NextPart = CurTime() + math.Rand(0, 0.02)
+		local vec = VectorRand() * 3
+		local pos = self:LocalToWorld(vec)
+		local particle = self.Emitter:Add( "particle/snow.vmt", pos)
+		particle:SetVelocity(  Vector(0,0, 4) )
+		particle:SetDieTime( 7 )
+		particle:SetStartAlpha( 140 )
+		particle:SetEndAlpha( 0 )
+		particle:SetStartSize( 5 )
+		particle:SetEndSize( 6 )   
+		particle:SetRoll( 0 )
+		particle:SetRollDelta( 0 )
+		particle:SetColor( 0, 0, 0 )
+		//particle:SetGravity( Vector( 0, 0, 10 ) )
+	end
+end
+
+function ENT:OnRemove()
+	self.Emitter:Finish()
 end
