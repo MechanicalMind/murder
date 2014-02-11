921a4c05fcd9f387d1d0f93877199e95fdcb379e
MechanicalMind <cyber_cam34@yahoo.com.au>
2014-02-11 14:25:17 +1100

    Knife charge disapears on holster

--- entities/weapons/weapon_mu_knife/init.lua
+++ entities/weapons/weapon_mu_knife/init.lua
@@ -14,6 +14,13 @@ function SWEP:Deploy()
 end
 
 function SWEP:Holster()
+	if IsValid(self.Owner) then
+		net.Start("mu_knife_charge")
+		net.WriteEntity(self)
+		net.WriteUInt(0, 8)
+		net.Send(self.Owner)
+	end
+	self.ChargeStart = nil
 	return true
 end
 
