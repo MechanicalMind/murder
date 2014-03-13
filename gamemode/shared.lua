621afc99d64883af4b33191b1c615314876c0989
MechanicalMind <cyber_cam34@yahoo.com.au>
2014-03-14 03:29:47 +1100

    Version 17

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,7 +4,7 @@ GM.Author 	= "MechanicalMind"
 // credits to Waddlesworth for the logo and menu icon
 GM.Email 	= ""
 GM.Website 	= "www.codingconcoctions.com/murder/"
-GM.Version = "16"
+GM.Version = "17"
 
 team.SetUp(1, "Spectators", Color(150, 150, 150))
 team.SetUp(2, "Players", Color(26, 120, 245))
