555b24026dca0335eb2d4d568513ea992f652bf6
VictorienXP <victorienxp@orange.fr>
2014-04-11 20:52:02 +0200

    Create french.lua
    A French translation !

---
+++ gamemode/lang/french.lua
@@ -0,0 +1,108 @@
+pt.bystander = "Innocent"
+pt.murderer = "Meurtrier"
+pt.spectator = "Spectateur"
+
+pt.knife = "Couteau"
+pt.magnum = "Magnum"
+pt.gun = "Pistolet"
+pt.hands = "Mains"
+
+
+pt.teamSpectators = "Spectateurs"
+pt.teamPlayers = "Joueurs"
+
+
+pt.killedTeamKill = "{player} a tué un innocent"
+pt.killedMurderer = "{player} a tué le meurtrier"
+
+pt.murdererDeathUnknown = "Le meurtrier est mort dans de mystérieuses circonstances"
+
+pt.changeTeam = "{player} a changé d'équipe vers {team}"
+pt.teamMoved = "{player} a été déplacé vers {team}"
+pt.teamMovedAFK = "{player} a été déplacé vers {team} pour avoir été AFK"
+
+pt.spectateFailed = "Vous ne pouvez pas observer. Vous n'êtes pas mort."
+
+pt.murdererDisconnect = "Le meurtrier a rage quit"
+pt.murdererDisconnectKnown = "Le meurtrier a rage quit, c'était {murderer}"
+
+pt.winBystanders = "Les innocents ont gagné !"
+pt.winBystandersMurdererWas = " Le meurtrier était {murderer}"
+pt.winMurderer = "Le meurtrier a gagné !"
+pt.winMurdererMurdererWas = " C'était {murderer}"
+
+pt.minimumPlayers = "Pas assez de joueurs pour commencer la partie"
+pt.roundStarted = "Une nouvelle partie a commencé"
+
+
+pt.adminMurdererSelect = "{player} sera le meurtrier au prochain tour"
+pt.adminMurdererForce = "Forcer à être le meurtrier"
+pt.adminSpectate = "Observer"
+pt.adminMoveToSpectate = "Déplacer vers {spectate}"
+
+pt.mapChange = "Changement de map vers {map}"
+
+pt.scoreboard = "Tableau des scores"
+pt.scoreboardName = "Pseudo"
+pt.scoreboardPing = "Ping"
+pt.scoreboardBystanderName = "Nom de jeu"
+pt.scoreboardStatus = "Status"
+pt.scoreboardChance = "Chance"
+pt.scoreboardRefresh = "Rafraichir"
+
+pt.scoreboardJoinTeam = "Rejoindre"
+
+pt.scoreboardActionAdmin = "Est un admin"
+pt.scoreboardActionMute = "Mute"
+pt.scoreboardActionUnmute = "Unmute"
+
+pt.endroundMurdererQuit = "Les innocents ont gagné ! Le meurtrier a rage quit"
+pt.endroundBystandersWin = "Les innocents ont gagné !"
+pt.endroundMurdererWins = "Le meurtrier a gagné !"
+pt.endroundMurdererWas = "Le meurtrier était {murderer}"
+
+pt.endroundLootCollected = "Objets ramassés"
+
+pt.adminPanel = "Menu Admin"
+
+pt.spectating = "Vous observez"
+
+pt.adMelonbomberWhy = "Pourquoi ne pas essayer "
+pt.adMelonbomberBy = " un mode de jeu par le créateur de Murder"
+
+pt.voiceHelp = "Aide"
+pt.voiceHelpDescription = "Cri d'aide"
+pt.voiceFunny = "Blague"
+pt.voiceFunnyDescription = "Un peu de blague"
+pt.voiceScream = "Crier"
+pt.voiceScreamDescription = "Comme une petite fille"
+pt.voiceMorose = "Morose"
+pt.voiceMoroseDescription = "Sentir la tristesse"
+
+
+pt.startHelpBystanderTitle = "Vous êtes un innocent"
+pt.startHelpBystander = {
+	"Il y a un meurtrier en cavale",
+	"Ne pas se faire tuer"
+}
+
+
+pt.startHelpGunTitle = "Vous êtes un innocent"
+pt.startHelpGunSubtitle = "avec une arme secrète"
+pt.startHelpGun = {
+	"Il y a un meurtrier en cavale",
+	"Ne pas se faire tuer"
+}
+
+pt.startHelpMurdererTitle = "Vous êtes le meurtrier"
+pt.startHelpMurderer = {
+	"Tuez tout le monde",
+	"Ne pas se faire remarquer"
+}
+
+pt.murdererFog = "Votre présence maléfique se montre"
+pt.murdererFogSub = "Tuer quelqu'un pour la cacher"
+
+pt.pressEToDisguiseFor1Loot = "[E] Se déguiser pour 1 objet"
+
+pt.playerStatusDead = "Mort"
