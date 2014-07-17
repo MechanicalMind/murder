f0746952f96801da2db28e29a97b0f62ca7d3799
Bruno Carvalho <comedinhass@gmail.com>
2014-07-16 23:59:23 -0300

    lang brazilian portuguese

---
+++ gamemode/lang/portuguese.lua
@@ -0,0 +1,108 @@
+pt.bystander = "Inocente"
+pt.murderer = "Assassino"
+pt.spectator = "Observador"
+
+pt.knife = "Faca"
+pt.magnum = "Revolver"
+pt.gun = "Arma"
+pt.hands = "Mãos"
+
+
+pt.teamSpectators = "Observadores"
+pt.teamPlayers = "Jogadores"
+pt.teamAss = "Bolo"
+
+pt.killedTeamKill = "{player} matou um inocente!"
+pt.killedMurderer = "{player} matou o assassino"
+
+pt.murdererDeathUnknown = "O assassino morreu de circunstacias misteriosas"
+
+pt.changeTeam = "{player} trocou de time para {team}"
+pt.teamMoved = "{player} foi movido para {team}"
+pt.teamMovedAFK = "{player} foi movido para {team} por ficar AFK"
+
+pt.spectateFailed = "Você não pode observar. Você não está morto."
+
+pt.murdererDisconnect = "O Assassino não aguentou a pressão e saiu."
+pt.murdererDisconnectKnown = "O Assassino saiu, ele era {murderer}"
+
+pt.winBystanders = "Os inocentes venceram!"
+pt.winBystandersMurdererWas = " o assassino era {murderer}"
+pt.winMurderer = "O Assassino venceu!"
+pt.winMurdererMurdererWas = " ele era {murderer}"
+
+pt.minimumPlayers = "Jogadores insuficientes para inicar a partida."
+pt.roundStarted = "Uma nova rodada começou"
+
+
+pt.adminMurdererSelect = "{player} será o assassino no prox. round"
+pt.adminMurdererForce = "Forçar assassino prox. round"
+pt.adminSpectate = "Observar"
+pt.adminMoveToSpectate = "Mover para {spectate}"
+
+pt.mapChange = "Trocando de mapa para {map}"
+
+pt.scoreboard = "Placar"
+pt.scoreboardName = "Nome"
+pt.scoreboardPing = "Ping"
+pt.scoreboardBystanderName = "Pseudónimo"
+pt.scoreboardStatus = "Status"
+pt.scoreboardChance = "Chance"
+pt.scoreboardRefresh = "Refresh"
+
+pt.scoreboardJoinTeam = "Entrar"
+
+pt.scoreboardActionAdmin = "É um Admin!"
+pt.scoreboardActionMute = "Silenciar"
+pt.scoreboardActionUnmute = "Dessilenciar"
+
+pt.endroundMurdererQuit = "Os Inocentes venceram! O Assassino não aguentou a pressão e saiu!"
+pt.endroundBystandersWin = "Inocentes venceram!"
+pt.endroundMurdererWins = "O Assassino venceu!"
+pt.endroundMurdererWas = "O assassino era {murderer}"
+
+pt.endroundLootCollected = "Loot Coletado"
+
+pt.adminPanel = "Painel de Administrador"
+
+pt.spectating = "Observando"
+
+pt.adMelonbomberWhy = "Porque não tentar "
+pt.adMelonbomberBy = " um gamemode do criador do Murder"
+
+pt.voiceHelp = "Ajuda"
+pt.voiceHelpDescription = "Grite por ajuda"
+pt.voiceFunny = "Piada"
+pt.voiceFunnyDescription = "Um pouco de humor"
+pt.voiceScream = "Gritar"
+pt.voiceScreamDescription = "Grite como uma garotinha"
+pt.voiceMorose = "Resmungar"
+pt.voiceMoroseDescription = "Sinta a tristeza"
+
+
+pt.startHelpBystanderTitle = "Você é um Inocente"
+pt.startHelpBystander = {
+	"Porém temos um assassino a solta",
+	"Não seja morto"
+}
+
+
+pt.startHelpGunTitle = "Você é um Inocente"
+pt.startHelpGunSubtitle = "com uma arma secreta"
+pt.startHelpGun = {
+	"Porém temos um assassino a solta",
+	"Ache e elimine ele"
+}
+
+pt.startHelpMurdererTitle = "Você é o Assassino"
+pt.startHelpMurderer = {
+	"Mate todos eles",
+	"Não seja pego"
+}
+
+pt.murdererFog = "Sua presença maliguina está se mostrando!"
+pt.murdererFogSub = "Mate alguem para se esconder"
+
+pt.pressEToDisguiseFor1Loot = "[E] Disfarce ( custa 1 loot )"
+
+pt.playerStatusDead = "Morto"
