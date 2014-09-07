47a8743efc9e42605873e4a541eeab22584c764f
Cristian <forzacristian@hotmail.it>
2014-09-07 13:11:23 +0200

    Italian language
    I've translated Murder from English to Italian, this is the .lua file. I left the term rage quit because it's used by italian gamers.

---
+++ gamemode/lang/italian.lua
@@ -0,0 +1,108 @@
+pt.bystander = "Innocente"
+pt.murderer = "Assassino"
+pt.spectator = "Spettatore"
+
+pt.knife = "Coltello"
+pt.magnum = "Magnum"
+pt.gun = "Arma"
+pt.hands = "Mani"
+
+
+pt.teamSpectators = "Spettatori"
+pt.teamPlayers = "Giocatori"
+pt.teamAss = "Torta"
+
+pt.killedTeamKill = "{player} ha ucciso un innocente"
+pt.killedMurderer = "{player} ha ucciso l'assassino"
+
+pt.murdererDeathUnknown = "L'assassino è morto in circostanze misteriose"
+
+pt.changeTeam = "{player} va nel gruppo {team}"
+pt.teamMoved = "{player} è stato spostato in {team}"
+pt.teamMovedAFK = "{player} è stato spostato in {team} essendo AFK"
+
+pt.spectateFailed = "Non puoi assistere. Non sei morto."
+
+pt.murdererDisconnect = "L'assassino rage quitta"
+pt.murdererDisconnectKnown = "L'assassino rage quitta, era {murderer}"
+
+pt.winBystanders = "Gli innocenti vincono!"
+pt.winBystandersMurdererWas = " L'assassino era {murderer}"
+pt.winMurderer = "L'assassino vince!!"
+pt.winMurdererMurdererWas = " Era {murderer}"
+
+pt.minimumPlayers = "Non ci sono abbastanza giocatori per iniziare il round"
+pt.roundStarted = "Un nuovo round è iniziato"
+
+
+pt.adminMurdererSelect = "{player} sarà l'assassino il prossimo round"
+pt.adminMurdererForce = "Forza la scelta dell'assassino"
+pt.adminSpectate = "Osserva"
+pt.adminMoveToSpectate = "Va a {spectate}"
+
+pt.mapChange = "Cambiando mappa verso {map}"
+
+pt.scoreboard = "Scoreboard"
+pt.scoreboardName = "Nome"
+pt.scoreboardPing = "Ping"
+pt.scoreboardBystanderName = "Nome del giocatore"
+pt.scoreboardStatus = "Status"
+pt.scoreboardChance = "Chance"
+pt.scoreboardRefresh = "Aggiorna"
+
+pt.scoreboardJoinTeam = "Entra"
+
+pt.scoreboardActionAdmin = "È un admin"
+pt.scoreboardActionMute = "Silenzia"
+pt.scoreboardActionUnmute = "Riattiva"
+
+pt.endroundMurdererQuit = "Gli innocenti vincono! L'assassino rage quitta"
+pt.endroundBystandersWin = "Gli innocenti vincono!"
+pt.endroundMurdererWins = "L'assassino vince!"
+pt.endroundMurdererWas = "L'assassino era {murderer}"
+
+pt.endroundLootCollected = "Oggetti raccolti"
+
+pt.adminPanel = "Pannello Admin"
+
+pt.spectating = "Osservando"
+
+pt.adMelonbomberWhy = "Perché non provare "
+pt.adMelonbomberBy = " Una gamemode dal creatore di Murder"
+
+pt.voiceHelp = "Aiuto"
+pt.voiceHelpDescription = "Richiesta di aiuto"
+pt.voiceFunny = "Scherzo"
+pt.voiceFunnyDescription = "Piccolo scherzo"
+pt.voiceScream = "Urlo"
+pt.voiceScreamDescription = "Come una ragazzina"
+pt.voiceMorose = "Tetro"
+pt.voiceMoroseDescription = "Senti la tristezza"
+
+
+pt.startHelpBystanderTitle = "Sei un innocente"
+pt.startHelpBystander = {
+	"C'è un assassino in libertà",
+	"Non farti uccidere"
+}
+
+
+pt.startHelpGunTitle = "Sei un innocente"
+pt.startHelpGunSubtitle = "con un'arma segreta"
+pt.startHelpGun = {
+	"C'è un assassino in libertà",
+	"Trovalo e uccidilo"
+}
+
+pt.startHelpMurdererTitle = "Sei l'assassino"
+pt.startHelpMurderer = {
+	"Uccidi tutti",
+	"Non farti beccare"
+}
+
+pt.murdererFog = "La tua presenza maligna si sta mostrando"
+pt.murdererFogSub = "Uccidi qualcuno per nasconderla"
+
+pt.pressEToDisguiseFor1Loot = "[E] Camuffa per 1 oggetto"
+
+pt.playerStatusDead = "Morto"
