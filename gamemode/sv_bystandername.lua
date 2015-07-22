cdc5fbd647a0c3022376a2d591bf14444501e484
MechanicalMind <cyber_cam34@yahoo.com.au>
2015-07-22 14:26:45 +1000

    Added gender support for bystander names

--- gamemode/sv_bystandername.lua
+++ gamemode/sv_bystandername.lua
@@ -1,30 +1,40 @@
-GM.BystanderNameParts = {"Alfa",
-"Bravo",
-"Charlie",
-"Delta",
-"Echo",
-"Foxtrot",
-"Golf",
-"Hotel",
-"India",
-"Juliett",
-"Kilo",
-"Lima",
-"Miko",
-"November",
-"Oscar",
-"Papa",
-"Quebec",
-"Romeo",
-"Sierra",
-"Tango",
-"Uniform",
-"Victor",
-"Whiskey",
-"X-ray",
-"Yankee",
-"Zulu"
-}
+
+
+GM.BystanderNameParts = {}
+local function addPart(name, sex)
+	local tab = {}
+	tab.name = name
+	tab.sex = sex
+	table.insert(GM.BystanderNameParts, tab)
+end
+
+/* Don't add to this list, add names to data/murder/bystander_name_parts.txt */
+addPart("Alfa")
+addPart("Bravo")
+addPart("Charlie")
+addPart("Delta")
+addPart("Echo")
+addPart("Foxtrot")
+addPart("Golf")
+addPart("Hotel")
+addPart("India")
+addPart("Juliett")
+addPart("Kilo")
+addPart("Lima")
+addPart("Miko")
+addPart("November")
+addPart("Oscar")
+addPart("Papa")
+addPart("Quebec")
+addPart("Romeo")
+addPart("Sierra")
+addPart("Tango")
+addPart("Uniform")
+addPart("Victor")
+addPart("Whiskey")
+addPart("X-ray")
+addPart("Yankee")
+addPart("Zulu")
 
 local PlayerMeta = FindMetaTable("Player")
 local EntityMeta = FindMetaTable("Entity")
@@ -32,13 +42,24 @@ local EntityMeta = FindMetaTable("Entity")
 GM.BystanderWords = CreateClientConVar( "mu_bystandername_words", 1, FCVAR_ARCHIVE, "Number of words to generate for bystander name" )
 
 // adds a name to the bystander parts generation table
-function GM:AddBystanderNamePart(name)
-	table.insert(self.BystanderNameParts, name)
+function GM:AddBystanderNamePart(name, sex)
+	name = tostring(name)
+	if !name then error("arg 1 must be a string") end
+	if sex != "male" && sex != "female" then sex = nil end
+	local tab = {}
+	tab.name = name
+	tab.sex = sex
+	table.insert(self.BystanderNameParts, tab)
 end
 
 // removes a name to the bystander parts generation table
-function GM:RemoveBystanderNamePart(name)
-	table.RemoveByValue(self.BystanderNameParts, name)
+function GM:RemoveBystanderNamePart(name, sex)
+	for k, v in pairs(self.BystanderNameParts) do
+		if v.name == name && v.sex == sex then
+			table.remove(self.BystanderNameParts, k)
+			break
+		end
+	end
 end
 
 // returns the bystander parts generation table
@@ -46,13 +67,19 @@ function GM:GetBystanderNameParts()
 	return self.BystanderNameParts
 end
 
-function GM:GenerateName(words)
+function GM:GenerateName(words, sex)
 	if #self.BystanderNameParts <= 0 then
 		return "error"
 	end
 	local name
 	for i = 1, words do
-		local word = self.BystanderNameParts[math.random(#self.BystanderNameParts)]
+		local tab = {}
+		for k, v in pairs(self.BystanderNameParts) do
+			if v.sex == sex || v.sex == nil then
+				table.insert(tab, v.name)
+			end
+		end
+		local word = tab[math.random(#tab)]
 		if !name then
 			name = word
 		else
@@ -63,12 +90,21 @@ function GM:GenerateName(words)
 end
 
 function GM:LoadBystanderNames()
-	local jason = file.ReadDataAndContent("murder/bystander_name_parts.txt")
-	if jason then
+	local text = file.ReadDataAndContent("murder/bystander_name_parts.txt")
+	if text then
 		local tbl = {}
 		local i = 1
-		for name in jason:gmatch("[^\r\n]+") do
-			table.insert(tbl, name)
+		for line in text:gmatch("[^\r\n]+") do
+			local name, sex = line:match("([^,]+)/([^,]+)")
+			if !name then name = line end
+			if sex == "m" then sex = "male" end
+			if sex == "f" then sex = "female" end
+			if sex != "male" && sex != "female" then sex = nil end
+			local tab = {}
+			tab.name = name
+			tab.sex = sex
+			table.insert(tbl, tab)
+			print("DEBUG Read bystander part", name, sex)
 		end
 		self.BystanderNameParts = tbl
 	end
@@ -76,7 +112,7 @@ end
 
 function EntityMeta:GenerateBystanderName()
 	local words = math.max(1, GAMEMODE.BystanderWords:GetInt())
-	local name = GAMEMODE:GenerateName(words)
+	local name = GAMEMODE:GenerateName(words, self.ModelSex or "male")
 	self:SetNWString("bystanderName", name)
 	self.BystanderName = name
 end
