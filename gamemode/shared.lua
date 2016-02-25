45f0b62c568421cfd1bd0aedb14c4987d4d409b1
MechanicalMind <cyber_cam34@yahoo.com.au>
2016-02-25 15:37:54 +1100

    Version 24

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,7 +4,7 @@ GM.Author 	= "MechanicalMind"
 // credits to Waddlesworth for the logo and menu icon
 GM.Email 	= ""
 GM.Website 	= "www.codingconcoctions.com/murder/"
-GM.Version = "23"
+GM.Version = "24"
 
 function GM:SetupTeams()
 	team.SetUp(1, translate.teamSpectators, Color(150, 150, 150))
