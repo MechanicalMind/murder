c18a0a403149fd055f81c37e11a2c147ec4f7443
Vitalij Mikhailov <Aragas@users.noreply.github.com>
2016-02-27 17:41:48 +0300

    Additional info on round end
    For addons like Pointshop 2, to get info in how round ended. Example here  
    https://github.com/Aragas/PS2-Murder-Integration/blob/master/lua/ps2/modules/mu_integration/sv_muhooks.lua#L38

--- gamemode/sv_rounds.lua
+++ gamemode/sv_rounds.lua
@@ -237,6 +237,7 @@ function GM:EndTheRound(reason, murderer)
 	self.MurdererLastKill = nil
 
 	hook.Call("OnEndRound")
+	hook.Call("OnEndRoundResult", reason)
 	self.RoundCount = self.RoundCount + 1
 	local limit = self.RoundLimit:GetInt()
 	if limit > 0 then
