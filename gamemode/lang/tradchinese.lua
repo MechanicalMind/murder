9451f0a4b0ba6111d1a4c4f0a714690082851ab4
MechanicalMind <cyber_cam34@yahoo.com.au>
2016-08-07 19:24:36 +1000

    Fixed chinese translation formmating

---
+++ gamemode/lang/tradchinese.lua
@@ -0,0 +1,116 @@
+pt.bystander = "旁觀者"
+pt.murderer = "兇手"
+pt.spectator = "觀眾"
+
+pt.knife = "刀"
+pt.magnum = "麥格農手槍"
+pt.gun = "槍"
+pt.hands = "手"
+
+
+pt.teamSpectators = "觀眾"
+pt.teamPlayers = "玩家"
+pt.teamAss = "蛋糕"
+
+pt.killedTeamKill = "{player}殺死一個無辜的旁觀者"
+pt.killedMurderer = "{player}殺了兇手"
+
+pt.murdererDeathUnknown = "兇手莫名其妙的死亡"
+
+pt.changeTeam = "{player}變更隊伍為{team}"
+pt.teamMoved = "{player}被變更為{team}"
+pt.teamMovedAFK = "{player}因為掛網被變更為{team}"
+
+pt.spectateFailed = "您不能當觀眾，您還沒死。"
+
+pt.murdererDisconnect = "兇手憤而中離"
+pt.murdererDisconnectKnown = "兇手{murderer}憤怒的離開遊戲"
+
+pt.winBystanders = "旁觀者贏了！"
+pt.winBystandersMurdererWas = "兇手是{murderer}"
+pt.winMurderer = "兇手獲勝！"
+pt.winMurdererMurdererWas = "兇手是{murderer}"
+
+pt.minimumPlayers = "沒有足夠的玩家開始一輪新遊戲"
+pt.roundStarted = "新一輪的開始"
+
+
+pt.adminMurdererSelect = "{player}將成為下一輪的兇手"
+pt.adminMurdererForce = "強制指定為下一輪的兇手"
+pt.adminSpectate = "旁觀"
+pt.adminMoveToSpectate = "移至{spectate}"
+
+pt.mapChange = "更換地圖為{map}"
+
+pt.scoreboard = "記分板"
+pt.scoreboardName = "名稱"
+pt.scoreboardPing = "Ping"
+pt.scoreboardBystanderName = "旁觀者名稱"
+pt.scoreboardStatus = "狀態"
+pt.scoreboardChance = "機會"
+pt.scoreboardRefresh = "更新"
+
+pt.scoreboardJoinTeam = "加入"
+
+pt.scoreboardActionAdmin = "管理員"
+pt.scoreboardActionMute = "靜音"
+pt.scoreboardActionUnmute = "取消靜音"
+pt.scoreboardActionViewProfile = "查看資料"
+
+pt.endroundMurdererQuit = "兇手憤而中離，旁觀者贏了！"
+pt.endroundBystandersWin = "旁觀者贏了！"
+pt.endroundMurdererWins = "兇手獲勝！"
+pt.endroundMurdererWas = "兇手是{murderer}"
+
+pt.endroundLootCollected = "戰利品收集"
+
+pt.adminPanel = "管理面板"
+
+pt.spectating = "旁觀中"
+
+pt.adMelonbomberWhy = "為何不試試"
+pt.adMelonbomberBy = "一個為謀殺而創造的遊戲模式"
+
+pt.voiceHelp = "求救"
+pt.voiceHelpDescription = "喊救命"
+pt.voiceFunny = "搞笑"
+pt.voiceFunnyDescription = "說一個笑話"
+pt.voiceScream = "吶喊"
+pt.voiceScreamDescription = "像個小女孩"
+pt.voiceMorose = "鬱悶"
+pt.voiceMoroseDescription = "感到悲傷"
+
+
+pt.startHelpBystanderTitle = "您是旁觀者"
+pt.startHelpBystander = {
+	"有一個不受控制的殺人犯",
+	"別被他殺了"
+}
+
+
+pt.startHelpGunTitle = "您是旁觀者"
+pt.startHelpGunSubtitle = "有秘密武器"
+pt.startHelpGun = {
+	"有一個不受控制的殺人犯",
+	"找到並殺死他"
+}
+
+pt.startHelpMurdererTitle = "您是殺人犯"
+pt.startHelpMurderer = {
+	"殺了所有的人",
+	"不要被逮住"
+}
+
+pt.murdererFog = "您的邪惡氣息外洩"
+pt.murdererFogSub = "殺一個人來隱藏"
+
+pt.pressEToDisguiseFor1Loot = "[E] 偽裝成另一個樣貌"
+
+pt.playerStatusDead = "死亡"
+
+//ttt_traitor_button 相容於 TTT 地圖
+pt.ttt_tbut_single = "一次性使用"
+pt.ttt_tbut_reuse = "可重複使用"
+pt.ttt_tbut_retime = "{num}秒後可再次使用"
+pt.ttt_tbut_waittime = "可重複使用 {timesec}"
+pt.ttt_tbut_help = "按{key}鍵使用"
