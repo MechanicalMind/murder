9635ccbe55b2f5d9da843dc0e0c54863cdb7b10f
MechanicalMind <cyber_cam34@yahoo.com.au>
2017-01-29 20:32:42 +1100

    Added turkish language

---
+++ gamemode/lang/turkish.lua
@@ -0,0 +1,116 @@
+pt.bystander = "Masum"
+pt.murderer = "Katil"
+pt.spectator = "İzleyici"
+
+pt.knife = "Bıçak"
+pt.magnum = "Magnum"
+pt.gun = "Silah"
+pt.hands = "Eller"
+
+
+pt.teamSpectators = "İzleyiciler"
+pt.teamPlayers = "Oyuncular"
+pt.teamAss = "Pasta"
+
+pt.killedTeamKill = "{player} bir masumu öldürdü."
+pt.killedMurderer = "{player} katili öldürmeyi başardı."
+
+pt.murdererDeathUnknown = "Katil bilinmeyen bir nedenden ötürü öldü."
+
+pt.changeTeam = "{player} {team} takımına geçti."
+pt.teamMoved = "{player} {team} takımına taşınıldı."
+pt.teamMovedAFK = "{player} AFK olduğu için {team} takımına taşınıldı."
+
+pt.spectateFailed = "Ölü değilsin. İzleyici olamazsın."
+
+pt.murdererDisconnect = "Katil rage quit attı."
+pt.murdererDisconnectKnown = "Murderer, namı diğer {murderer} rage quit attı."
+
+pt.winBystanders = "Masumlar kazandı!"
+pt.winBystandersMurdererWas = " {murderer} katil rolündeydi."
+pt.winMurderer = "Katil kazandı!"
+pt.winMurdererMurdererWas = " Onun adı buydu: {murderer}"
+
+pt.minimumPlayers = "Randın başlaması için yeterince oyuncu yok."
+pt.roundStarted = "Yeni bir rand başladı."
+
+
+pt.adminMurdererSelect = "{player} sıradaki rand katil olacak."
+pt.adminMurdererForce = "Sıradaki el zorla katil yap."
+pt.adminSpectate = "İzle"
+pt.adminMoveToSpectate = "{spectate} takımına taşı."
+
+pt.mapChange = "{map} haritasına geçiliyor."
+
+pt.scoreboard = "Skor Tablosu"
+pt.scoreboardName = "İsim"
+pt.scoreboardPing = "Gecikme"
+pt.scoreboardBystanderName = "Masum Adı"
+pt.scoreboardStatus = "Durum"
+pt.scoreboardChance = "Şans"
+pt.scoreboardRefresh = "Yenile"
+
+pt.scoreboardJoinTeam = "Katıl"
+
+pt.scoreboardActionAdmin = "O bir admin."
+pt.scoreboardActionMute = "Sustur"
+pt.scoreboardActionUnmute = "Sesi aç"
+pt.scoreboardActionViewProfile = "Profiline bak"
+
+pt.endroundMurdererQuit = "Masumlar kazandı! Katil oyundan kaçtı!"
+pt.endroundBystandersWin = "Masumlar kazandı!"
+pt.endroundMurdererWins = "Katil kazandı!"
+pt.endroundMurdererWas = "{murderer} katil rolündeydi."
+
+pt.endroundLootCollected = "Toplanan Ganimet"
+
+pt.adminPanel = "Admin Paneli"
+
+pt.spectating = "İzleniyor"
+
+pt.adMelonbomberWhy = "Murder'ın yapımcısından "
+pt.adMelonbomberBy = " başka bir mod denemeye ne dersin?"
+
+pt.voiceHelp = "Yardım"
+pt.voiceHelpDescription = "Yardım için bağır!"
+pt.voiceFunny = "Komik"
+pt.voiceFunnyDescription = "Biraz espiri"
+pt.voiceScream = "Çığlık"
+pt.voiceScreamDescription = "Küçük bir kız gibi."
+pt.voiceMorose = "Mutsuz"
+pt.voiceMoroseDescription = "Mutsuzluğu hisset."
+
+
+pt.startHelpBystanderTitle = "Sen bir masumsun."
+pt.startHelpBystander = {
+	"Etrafta bir katil dolanmakta",
+	"hayatta kalmaya çalış!"
+}
+
+
+pt.startHelpGunTitle = "Sen elinde gizli bir silahı"
+pt.startHelpGunSubtitle = "olan özel birisin."
+pt.startHelpGun = {
+	"Etrafta bir katil dolanmakta",
+	"onu bul ve öldür!"
+}
+
+pt.startHelpMurdererTitle = "Sen katilsin."
+pt.startHelpMurderer = {
+	"Herkesi öldür",
+	"ve yakalanma!"
+}
+
+pt.murdererFog = "Şeytani varlığın kendini gösteriyor."
+pt.murdererFogSub = "Gizlenmek için birini öldürmelisin."
+
+pt.pressEToDisguiseFor1Loot = "[E] 1 ganimet karşılığı kılık değiştir."
+
+pt.playerStatusDead = "Ölü"
+
+// ttt_traitor_button compatibility for TTT maps
+pt.ttt_tbut_single  = "Tek kullanım"
+pt.ttt_tbut_reuse   = "Tekrar kullanılabilir"
+pt.ttt_tbut_retime  = "{num} saniye içinde tekrar kullanılabilir."
+pt.ttt_tbut_waittime  = "{timesec} kadar süre içinde tekrar kullanılabilir."
+pt.ttt_tbut_help    = "{key} tuşu ile aktifleştirin."
