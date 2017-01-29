1b157c36439442980c74130847f522dc3413b710
MechanicalMind <cyber_cam34@yahoo.com.au>
2017-01-29 20:32:55 +1100

    Hand fix

--- gamemode/sv_player.lua
+++ gamemode/sv_player.lua
@@ -49,32 +49,7 @@ function GM:PlayerSpawn( ply )
 
 	ply:CalculateSpeed()
 
-	local oldhands = ply:GetHands()
-	if ( IsValid( oldhands ) ) then oldhands:Remove() end
-
-	local hands = ents.Create( "gmod_hands" )
-	if ( IsValid( hands ) ) then
-		ply:SetHands( hands )
-		hands:SetOwner( ply )
-
-		-- Which hands should we use?
-		local cl_playermodel = ply:GetInfo( "cl_playermodel" )
-		local info = player_manager.TranslatePlayerHands( cl_playermodel )
-		if ( info ) then
-			hands:SetModel( info.model )
-			hands:SetSkin( info.skin )
-			hands:SetBodyGroups( info.body )
-		end
-
-		-- Attach them to the viewmodel
-		local vm = ply:GetViewModel( 0 )
-		hands:AttachToViewmodel( vm )
-
-		vm:DeleteOnRemove( hands )
-		ply:DeleteOnRemove( hands )
-
-		hands:Spawn()
-	end
+	ply:SetupHands()
 
 	local spawnPoint = self:PlayerSelectTeamSpawn(ply:Team(), ply)
 	if IsValid(spawnPoint) then
