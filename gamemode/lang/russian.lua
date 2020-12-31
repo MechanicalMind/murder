e6da8b3d18b466d094fe9e9afce5bfd622179fcb
Blueberryy <igory.ygr200@gmail.com>
2020-12-31 20:29:24 +0500

    Update russian.lua

--- gamemode/lang/russian.lua
+++ gamemode/lang/russian.lua
@@ -1,7 +1,9 @@
 --[[
 Encoding must be UTF-8, or you will get "�".
 	Translated by Fixator10 
+	Updated by berry
 	http://vk.com/fixator10 | http://steamcommunity.com/id/fixator10 | https://telegram.me/fixator10
+	http://steamcommunity.com/profiles/76561198158864042
 --]]
 
 pt.bystander = "Свидетель"
@@ -24,12 +26,12 @@ pt.killedMurderer = "{player} убил убийцу"
 pt.murdererDeathUnknown = "Убийца умер при странном стечении обстоятельств"
 
 pt.changeTeam = "{player} сменил команду на \"{team}\""
-pt.teamMoved = "{player} был перемещен в {team}"
-pt.teamMovedAFK = "{player} был перемещен в {team} за пребывание в AFK"
+pt.teamMoved = "{player} был перемещён в команду {team}"
+pt.teamMovedAFK = "{player} был перемещён в команду {team} за бездействие"
 
 pt.spectateFailed = "Вы не можете наблюдать. Вы живы."
 
-pt.murdererDisconnect = "У убийцы RageQuit!"
+pt.murdererDisconnect = "Убийца вышел из игры в ярости!"
 pt.murdererDisconnectKnown = "Убийца покинул сервер, его звали {murderer}"
 
 pt.winBystanders = "Свидетели победили!"
@@ -39,6 +41,7 @@ pt.winMurdererMurdererWas = " Его звали {murderer}"
 
 pt.minimumPlayers = "Недостаточно игроков для начала игры"
 pt.waitingToStart = "Ожидание начала раунда"
+pt.roundStartsInTime = "Раунд начнётся через {seconds}"
 pt.roundStarted = "Новый раунд начался"
 
 
@@ -51,7 +54,7 @@ pt.mapChange = "Смена карты на {map}"
 
 pt.scoreboard = "Таблица игроков"
 pt.scoreboardName = "Имя"
-pt.scoreboardPing = "Ping"
+pt.scoreboardPing = "Пинг"
 pt.scoreboardBystanderName = "Имя свидетеля"
 pt.scoreboardStatus = "Статус"
 pt.scoreboardChance = "Шанс"
@@ -61,10 +64,10 @@ pt.scoreboardJoinTeam = "Присоединиться"
 
 pt.scoreboardActionAdmin = "Администратор"
 pt.scoreboardActionMute = "Заглушить"
-pt.scoreboardActionUnmute = "Снять заглушку"
+pt.scoreboardActionUnmute = "Снять заглушение"
 pt.scoreboardActionViewProfile = "Просмотреть профиль"
 
-pt.endroundMurdererQuit = "Свидетели победили! Убийца вышел"
+pt.endroundMurdererQuit = "Свидетели победили! Убийца вышел из игры"
 pt.endroundBystandersWin = "Свидетели победили!"
 pt.endroundMurdererWins = "Убийца победил!"
 pt.endroundMurdererWas = "Убийцей был {murderer}"
@@ -77,6 +80,7 @@ pt.spectating = "Наблюдение"
 
 pt.adMelonbomberWhy = "Почему бы не попробовать "
 pt.adMelonbomberBy = " игровой режим от создателя Murder"
+
 pt.voiceHelp = "Помогите"
 pt.voiceHelpDescription = "Позвать на помощь"
 pt.voiceFunny = "Шутка"
@@ -84,7 +88,7 @@ pt.voiceFunnyDescription = "Немного пошутить"
 pt.voiceScream = "Крик"
 pt.voiceScreamDescription = "Как маленькая девочка"
 pt.voiceMorose = "Мрачность"
-pt.voiceMoroseDescription = "Все очень плохо"
+pt.voiceMoroseDescription = "Всё очень плохо"
 
 
 pt.startHelpBystanderTitle = "Вы свидетель"
@@ -108,14 +112,15 @@ pt.startHelpMurderer = {
 }
 
 pt.murdererFog = "Ваша злая сущность стала видимой"
-pt.murdererFogSub = "Убейте кого-нибудь чтобы спрятаться"
+pt.murdererFogSub = "Убейте кого-нибудь, чтобы спрятаться"
 
 pt.pressEToDisguiseFor1Loot = "[E] Замаскироваться за 1 улику"
 
-pt.playerStatusDead = "Мертв"
+pt.playerStatusDead = "Мёртв"
 
 // ttt_traitor_button compatibility for TTT maps
 pt.ttt_tbut_single  = "Одноразово"
 pt.ttt_tbut_reuse   = "Многоразово"
 pt.ttt_tbut_retime  = "Многоразовое: через {num} сек"
+pt.ttt_tbut_waittime  = "Многоразовое: через {timesec}"
 pt.ttt_tbut_help    = "Нажмите {key} для активации"
