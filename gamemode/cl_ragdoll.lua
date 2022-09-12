9da1a67413cbc9409fb537fc683777b9edbee8d5
tehya-debug <97810690+tehya-debug@users.noreply.github.com>
2022-09-12 10:19:27 +0300

    Update cl_ragdoll.lua

--- gamemode/cl_ragdoll.lua
+++ gamemode/cl_ragdoll.lua
@@ -1,24 +1,26 @@
-local PlayerMeta = FindMetaTable("Player")
-local EntityMeta = FindMetaTable("Entity")
+local PLAYER = FindMetaTable("Player")
+local ENTITY = FindMetaTable("Entity")
 
-if !PlayerMeta.GetRagdollEntityOld then
-	PlayerMeta.GetRagdollEntityOld = PlayerMeta.GetRagdollEntity
-end
-function PlayerMeta:GetRagdollEntity()
+PLAYER.GetRagdollEntityOld = PLAYER.GetRagdollEntityOld or PLAYER.GetRagdollEntity
+
+function PLAYER:GetRagdollEntity()
 	local ent = self:GetNWEntity("DeathRagdoll")
+
 	if IsValid(ent) then
 		return ent
 	end
+
 	return self:GetRagdollEntityOld()
 end
 
-if !PlayerMeta.GetRagdollOwnerOld then
-	PlayerMeta.GetRagdollOwnerOld = PlayerMeta.GetRagdollOwner
-end
-function EntityMeta:GetRagdollOwner()
+ENTITY.GetRagdollOwnerOld = ENTITY.GetRagdollOwnerOld or ENTITY.GetRagdollOwner
+
+function ENTITY:GetRagdollOwner()
 	local ent = self:GetNWEntity("RagdollOwner")
+
 	if IsValid(ent) then
 		return ent
 	end
+
 	return self:GetRagdollOwnerOld()
 end
